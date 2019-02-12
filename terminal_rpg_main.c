b111c583872ab7ba7670fe7da64b0a15251a4ece
CYBERSHOU <mmttb@live.com.pt>
2019-02-12 17:44:02 -0100

    Added music to the background using mplayer

--- terminal_rpg_main.c
+++ terminal_rpg_main.c
@@ -18,28 +18,36 @@
 #include <stdlib.h>
 #include <ncurses.h>
 #include <unistd.h>
-#include <pulse/pulseaudio.h>
+#include <signal.h>
 
 #include "terminal_rpg.h"
 
 
 int main (int argc, char *argv[]) {
 
-    initscr();
-    keypad(stdscr, TRUE);
-    noecho();
-    curs_set(0);
+    pid_t pid = fork();
+    if(pid < 0)
+        return 1;
 
-    // system("mplayer -shuffle -loop 0 ~/Documents/project_terminal_rpg/GIT/TermRPG/audio_tracks/*.mp3 &> audio_tracks/log.txt & -- ");
+    if(pid > 0) {
+        initscr();
+        keypad(stdscr, TRUE);
+        noecho();
+        curs_set(0);
+        terminal_rpg_menu();
+        clear();
+        printw("Exiting...");
+        refresh();
+        kill(pid+2,SIGHUP);
+        sleep(1);
+        endwin();
 
-    terminal_rpg_menu();
-    clear();
-    printw("Exiting...");
-    refresh();
-    sleep(1);
-    endwin();
-
-    return 0;
+        return 0;
+    }
+    else {
+        system("mplayer -shuffle -loop 0 audio_tracks/*.mp3 &> audio_tracks/log.txt &");
+        exit(0);
+    }
 }
 
 //eof
